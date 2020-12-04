9dec353859281658339689098be5b7d4d57a2f8d
Andrew Zick <aezick@umich.edu>
2020-12-03 22:07:49 -0800

    day4

--- day4/puzzle1.py
+++ day4/puzzle1.py
@@ -1,5 +1,17 @@
 import os
 
 with open("input.txt", "r") as f:
+	passport = ""
+	count = 0
+
 	for line in f:
-		pass
+		if line != "\n":
+			passport += line
+			continue
+		else:
+			if "byr:" in passport and "iyr:" in passport and "eyr:" in passport and "hgt:" in passport:
+				if "hcl:" in passport and "ecl:" in passport and "pid:" in passport: 
+					count += 1
+			passport = ""
+
+	print(count)
